9855f1b82e29ff694e7e55cdecc0fea651cdf929
yceline-yu <cellystuff5@gmail.com>
2021-05-27 15:53:37 -0700

    iinitial commit

---
+++ src/TodoApp.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import TopTodo from "./TopTodo";
+import EditableTodoList from "./EditableTodoList";
+import TodoForm from "./TodoForm"
+
+/** App for managing a todo list.
+ *
+ * Props:
+ * - initialTodos: possible array of [ todo, ... ]
+ *
+ * State:
+ * - todos: array of [ todo, ... ]
+ *
+ * App -> TodoApp -> { TodoForm, EditableTodoList }
+ */
+
+function TodoApp({ initialTodos }) {
+  const [todos, setTodos] = useState(initialTodos) //array of objects
+
+  /** add a new todo to list */
+  function create(newTodo) {
+    let newDo = {...newTodo, id: uuid()}
+    setTodos(todos => [...todos, newDo])
+  }
+
+  /** update a todo with updatedTodo */
+  function update(updatedTodo) {
+    setTodos(todos =>
+      // const todosCopy = [...todos];
+      todos.map(todo => (
+        todo.id === updatedTodo.id
+      ? updatedTodo
+      : todo
+      )));
+}
+
+
+  /** delete a todo by id */
+  function remove(id) {
+    setTodos(todos => todos.filter(todo => todo.id !== id))
+  }
+
+  return (
+      <main className="TodoApp">
+        <div className="row">
+
+          <div className="col-md-6">
+            <EditableTodoList update={update} remove={remove} todos={todos}/> OR
+            <span className="text-muted">You have no todos.</span>
+          </div>
+
+          <div className="col-md-6">
+            {todos.length === 0 
+            ? null 
+            :<section className="mb-4">
+              <h3>Top Todo</h3>
+              <TopTodo todos={todos}/>
+            </section>}
+
+            <section>
+              <h3 className="mb-3">Add Nü</h3>
+              <TodoForm handleSave={create}/>
+            </section>
+          </div>
+
+        </div>
+      </main>
+  );
+}
+
+export default TodoApp;
